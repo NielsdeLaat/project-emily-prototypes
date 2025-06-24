95d8a38ff933efe9ec4a0427e0d50dcc2c893eeb
Niels <118165490+NielsdeLaat@users.noreply.github.com>
2025-06-24 13:26:06 +0200

    fixed avatar file names

--- chat-interface/src/config/avatars.ts
+++ chat-interface/src/config/avatars.ts
@@ -1,9 +1,9 @@
 export const PERSONA_AVATARS: Record<string, string> = {
   emily: "/avatars/emily.jpg",
   jason: "/avatars/jason.jpg",
-  kadir: "/avatars/Kadir.png",
-  Sae_byeok: "/avatars/Sae_Byeok.png",
-  mugisha: "/avatars/Mugisha.png",
+  kadir: "/avatars/kadir.png",
+  Sae_byeok: "/avatars/sae.png",
+  mugisha: "/avatars/mugisha.png",
   tyrone: "/avatars/tyrone.png",
-  fabrice: "/avatars/Frabrice.png",
+  fabrice: "/avatars/frabrice.png",
 };
